97e7ccca8c201e263c683bb9398cfeae9310624d
Dipen <dipen.ec2010@gmail.com>
2018-03-20 21:42:25 +0800

    commit

---
+++ testscript.rb
@@ -0,0 +1,13 @@
+class Testscript < Formula
+  desc ""
+  homepage ""
+  url "https://www.dropbox.com/s/1ie8jesqjesixgo/syncthing-macosx-amd64-v0.14-0.07.zip"
+  version "1.0.0"
+  sha256 "6dc72e65c8fdd32821bcc6cf832cb971f0ee812d58bb7605c55e89ea6c0ed0e2"
+
+
+  def install
+	bin.install "syncthing"
+  end
+
+end
