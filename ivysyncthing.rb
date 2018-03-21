44990ea826c891b731749e58f5bfb680a0d99d96
Dipen <dipen.ec2010@gmail.com>
2018-03-21 21:17:00 +0800

    added plist file

--- ivysyncthing.rb
+++ ivysyncthing.rb
@@ -1,7 +1,7 @@
 class Ivysyncthing < Formula
   desc ""
   homepage ""
-  url "https://github.com/dipenpatel235/ivysyncthing/archive/v1.0.0.tar.gz"
+  url "https://github.com/dipenpatel235/ivysyncthing/archive/v1.0.1.tar.gz"
   version "1.0.0"
   sha256 "1bdd5a8ea1e1e3a2a26836531d5170ccca010d2a68aaea67eebf32eafe929ff1"
 
